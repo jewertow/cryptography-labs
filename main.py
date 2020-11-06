0677947c4fb5817fd580609de171d9c959f76a42
Jacek Ewertowski <jacek.ewertowski1@gmail.com>
2020-11-07 00:19:11 +0100

    Refaktoryzacja

--- main.py
+++ main.py
@@ -3,73 +3,75 @@ from Crypto.Cipher import AES
 BLOCK_SIZE = 16
 
 
-def encrypt_ecb(msg: bytes, key_str: str) -> bytes:
-    key = str.encode(key_str)
-    cipher = AES.new(key, AES.MODE_ECB)
-    return cipher.encrypt(msg)
+class FixedSizeBlockAES:
+    def __init__(self, key: bytes, block_size: int):
+        self.key = key
+        self.block_size = block_size
+
+    def encrypt(self, msg: bytes) -> bytes:
+        pass
+
+    def decrypt(self, msg: bytes) -> bytes:
+        pass
+
+    def execute(self, input_file: str, encrypted_file: str, decrypted_file: str):
+        self.transform_file(input_file, encrypted_file, self.encrypt)
+        self.transform_file(encrypted_file, decrypted_file, self.decrypt)
+
+    def transform_file(self, input_file: str, output_file: str, aes_fn):
+        with open(input_file, 'rb') as f:
+            file_content = f.read()
+        blocks = self._to_blocks(file_content)
+        partial_results = []
+        for block in blocks:
+            block_res = aes_fn(block)
+            partial_results.append(block_res)
+        result = b''.join(partial_results)
+        with open(output_file, 'wb') as f:
+            f.write(result)
+
+    def _to_blocks(self, string):
+        return [string[i:i + self.block_size] for i in range(0, len(string), self.block_size)]
 
 
-def encrypt_cbc(msg: bytes, key_str: str) -> bytes:
-    key = str.encode(key_str)
-    cipher = AES.new(key, AES.MODE_CBC, iv=b'1111111111111111')
-    return cipher.encrypt(msg)
+class ECB(FixedSizeBlockAES):
+    def encrypt(self, msg: bytes) -> bytes:
+        cipher = AES.new(self.key, AES.MODE_ECB)
+        return cipher.encrypt(msg)
+
+    def decrypt(self, msg: bytes) -> bytes:
+        cipher = AES.new(self.key, AES.MODE_ECB)
+        return cipher.decrypt(msg)
 
 
-def decrypt_ecb(msg: bytes, key_str: str) -> bytes:
-    key = str.encode(key_str)
-    cipher = AES.new(key, AES.MODE_ECB)
-    return cipher.decrypt(msg)
+class CBC(FixedSizeBlockAES):
+    def __init__(self, key: bytes, block_size: int, iv: bytes):
+        super(CBC, self).__init__(key, block_size)
+        self.iv = iv
 
+    def encrypt(self, msg: bytes) -> bytes:
+        cipher = AES.new(self.key, AES.MODE_CBC, self.iv)
+        return cipher.encrypt(msg)
 
-def decrypt_cbc(msg: bytes, key_str: str) -> bytes:
-    key = str.encode(key_str)
-    cipher = AES.new(key, AES.MODE_CBC, iv=b'1111111111111111')
-    return cipher.decrypt(msg)
-
-
-def encrypt_file(input_file_name: str, output_file_name: str, key: str, encrypt):
-    with open(input_file_name, 'rb') as f:
-        plaintext = f.read()
-    blocks = _to_blocks(plaintext)
-    encrypted_blocks = []
-    for block in blocks:
-        enc_block = encrypt(block, key)
-        encrypted_blocks.append(enc_block)
-    result = b''.join(encrypted_blocks)
-    with open(output_file_name, 'wb') as f:
-        f.write(result)
-
-
-def decrypt_file(input_file_name: str, output_file_name: str, key: str, decrypt):
-    with open(input_file_name, 'rb') as f:
-        encrypted = f.read()
-    blocks = _to_blocks(encrypted)
-    decrypted_blocks = []
-    for block in blocks:
-        dec_block = decrypt(block, key)
-        decrypted_blocks.append(dec_block)
-    result = b''.join(decrypted_blocks)
-    with open(output_file_name, 'wb') as f:
-        f.write(result)
-
-
-def _to_blocks(string):
-    return [string[i:i+BLOCK_SIZE] for i in range(0, len(string), BLOCK_SIZE)]
+    def decrypt(self, msg: bytes) -> bytes:
+        cipher = AES.new(self.key, AES.MODE_CBC, self.iv)
+        return cipher.decrypt(msg)
 
 
 def main():
     alg = 'ECB'
-    key = 'Sixteen byte key'
-    prefix_name = '1Kb'
-    input_file_name = f'input/{prefix_name}.txt'
-    encrypted_file_name = f'{prefix_name}-{alg}.enc.txt'
-    decrypted_file_name = f'{prefix_name}-{alg}.dec.txt'
+    iv = b'1111111111111111'
+    key = b'Sixteen byte key'
+    prefix_name = '200Mb'
+    input_file = f'input/{prefix_name}.txt'
+    encrypted_file = f'{prefix_name}-{alg}.enc.txt'
+    decrypted_file = f'{prefix_name}-{alg}.dec.txt'
     if alg == 'ECB':
-        encrypt_file(input_file_name, encrypted_file_name, key, encrypt=encrypt_ecb)
-        decrypt_file(encrypted_file_name, decrypted_file_name, key, decrypt=decrypt_ecb)
+        ecb = ECB(key, BLOCK_SIZE)
+        ecb.execute(input_file, encrypted_file, decrypted_file)
     elif alg == 'CBC':
-        encrypt_file(input_file_name, encrypted_file_name, key, encrypt=encrypt_cbc)
-        decrypt_file(encrypted_file_name, decrypted_file_name, key, decrypt=decrypt_cbc)
+        cbc = CBC(key, BLOCK_SIZE, iv)
+        cbc.execute(input_file, encrypted_file, decrypted_file)
     else:
         raise RuntimeError(f'Unknown algorithm: {alg}')
 
