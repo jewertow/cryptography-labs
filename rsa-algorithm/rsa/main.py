73a6dc388d30a3c453876020e2020c4130764a15
Jacek Ewertowski <jacek.ewertowski1@gmail.com>
2020-12-05 09:11:26 +0100

    Remove object-oriented version of RSA

--- rsa-algorithm/rsa/main.py
+++ rsa-algorithm/rsa/main.py
@@ -1,21 +1,19 @@
-from rsa import RSA
+from rsa import rsa, encrypt, decrypt
 from sympy import nextprime
 
 
 def main():
     p = nextprime(1000)
     q = nextprime(p)
-    rsa = RSA(p, q)
+    private_key, public_key = rsa(p, q)
     print(f'p = {p}; q = {q}')
-    private_key = rsa.get_private_key()
-    print(f'private key = {private_key}')
-    public_key = rsa.get_public_key()
-    print(f'public key = {public_key}')
+    print(f'private key = ({private_key.d}, {private_key.n})')
+    print(f'public key = ({public_key.e}, {public_key.n})')
+
     message = 'Lorem ipsum dolor sit amet, consectetur tincidunt.'
-    print(f'message: {message}')
-    encrypted = rsa.encrypt(message)
+    encrypted = encrypt(public_key, message)
     print(f'encrypted message: {encrypted}')
-    decrypted = rsa.decrypt(encrypted)
+    decrypted = decrypt(private_key, encrypted)
     print(f'decrypted message: {decrypted}')
 
 
--- rsa-algorithm/rsa/rsa.py
+++ rsa-algorithm/rsa/rsa.py
@@ -2,47 +2,54 @@ from sympy import nextprime
 from math import gcd
 
 
-class RSA:
-    def __init__(self, p, q):
-        self.p = p
-        self.q = q
-        self.n = p * q
-        self.phi = (p - 1) * (q - 1)
-        self.e = find_relatively_prime(self.phi)
-        self.d = self._find_d()
-        self.block_size = len(str(self.n)) - 1
-        print(f'phi = {self.phi}, block_size = {self.block_size}')
+class PrivateKey:
+    def __init__(self, d, n):
+        self.d = d
+        self.n = n
 
-    def get_private_key(self) -> (int, int):
-        return self.d, self.n
 
-    def get_public_key(self) -> (int, int):
-        return self.e, self.n
+class PublicKey:
+    def __init__(self, e, n):
+        self.e = e
+        self.n = n
 
-    def encrypt(self, message: str) -> str:
-        result = ''
-        for char in message:
-            # c = m ^ e mod n
-            enc = (ord(char) ** self.e) % self.n
-            enc = str(enc).ljust(self.block_size)
-            result += str(enc)
-        return result
 
-    def decrypt(self, encrypted_msg: str) -> str:
-        result = ''
-        for i in range(0, len(encrypted_msg), self.block_size):
-            block = encrypted_msg[i: i + self.block_size]
-            block = block.strip()
-            # m = c ^ d mod n
-            dec = (int(block) ** self.d) % self.n
-            result += str(chr(dec))
-        return result
+def rsa(p: int, q: int) -> (PrivateKey, PublicKey):
+    n = p * q
+    phi = (p - 1) * (q - 1)
+    e = find_relatively_prime(phi)
+    d = _find_d(e, phi)
+    return PrivateKey(d, n), PublicKey(e, n)
 
-    def _find_d(self):
-        d = 2
-        while (self.e * d - 1) % self.phi != 0:
-            d += 1
-        return d
+
+def encrypt(pub_key: PublicKey, message: str) -> str:
+    block_size = len(str(pub_key.n - 1))
+    result = ''
+    for char in message:
+        # c = m ^ e mod n
+        enc = (ord(char) ** pub_key.e) % pub_key.n
+        enc = str(enc).ljust(block_size)
+        result += str(enc)
+    return result
+
+
+def decrypt(priv_key: PrivateKey, encrypted_msg: str) -> str:
+    block_size = len(str(priv_key.n - 1))
+    result = ''
+    for i in range(0, len(encrypted_msg), block_size):
+        block = encrypted_msg[i: i + block_size]
+        block = block.strip()
+        # m = c ^ d mod n
+        dec = (int(block) ** priv_key.d) % priv_key.n
+        result += str(chr(dec))
+    return result
+
+
+def _find_d(e: int, phi: int) -> int:
+    d = 2
+    while (e * d - 1) % phi != 0:
+        d += 1
+    return d
 
 
 # FIXME: ta funkcja szuka tylko większej od x, a mniejsza liczba też może być względnie pierwsza
